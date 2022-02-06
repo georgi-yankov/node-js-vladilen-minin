9e1c255447e5b992a1ed0bbcd37a680e24ce74fe
Georgi Yankov <georgi.st.yankov@gmail.com>
2022-02-06 11:35:25 +0200

    change PORT

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ const server = http.createServer((req, res) => {
   })
 })
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3002
 
 server.listen(PORT, () => {
   console.log(`Server has been started on ${PORT}...`)
